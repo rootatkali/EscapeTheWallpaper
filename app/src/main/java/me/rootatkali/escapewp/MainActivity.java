a3e0e54fa6f687136ba03105df27798af65bb7f2
Rotem <rotem@faceco.de>
2020-12-30 11:43:05 +0200

    Add initial framework

--- app/src/main/java/me/rootatkali/escapewp/MainActivity.java
+++ app/src/main/java/me/rootatkali/escapewp/MainActivity.java
@@ -2,13 +2,62 @@ package me.rootatkali.escapewp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.Map;
+
+import me.rootatkali.escapewp.mechanics.Action;
+import me.rootatkali.escapewp.mechanics.Node;
+
 public class MainActivity extends AppCompatActivity {
+  private int score;
+  private Map<Integer, Node> nodes;
+  private SharedPreferences prefs;
+  private Node current;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    
+    initializeNodes();
+    prefs = getPreferences(Context.MODE_PRIVATE);
+    score = prefs.getInt(getString(R.string.score_key), 0);
+    current = nodes.get(prefs.getInt(getString(R.string.current_node_key), 0));
+    
+  }
+  
+  private void initializeNodes() {
+    Node head = new Node(0, "You started Escape. Your eyes are closed.");
+    Node n1 = new Node(1, "You are in a dark room.\n" +
+        "There is some light coming from the window. You see a light switch.");
+    
+    head.addOptions(new Action(0, R.string.n0a0, 0, 1));
+    
+    nodes.put(0, head);
+    nodes.put(1, n1);
+  }
+  
+  private Node next(int optionId) {
+    Action action = Arrays.stream(current.getOptions()).filter(a -> a.getId() == optionId)
+        .findFirst().orElseThrow(RuntimeException::new);
+    score += action.getScore();
+    Node next = nodes.get(action.getNextNode());
+    
+    prefs.edit()
+        .putInt(getString(R.string.score_key), score)
+        .putInt(getString(R.string.current_node_key), next.getId())
+        .apply();
+  
+    try {
+      Thread.sleep(100);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    
+    return next;
   }
 }
---
+++ app/src/main/java/me/rootatkali/escapewp/mechanics/Action.java
@@ -0,0 +1,61 @@
+package me.rootatkali.escapewp.mechanics;
+
+import com.google.common.base.MoreObjects;
+
+import java.util.Objects;
+
+public class Action {
+  private final int id;
+  private final int messageId;
+  private final int score;
+  private final int nextNode;
+  
+  public Action(int id, int messageId, int score, int nextNode) {
+    this.id = id;
+    this.messageId = messageId;
+    this.score = score;
+    this.nextNode = nextNode;
+  }
+  
+  public int getId() {
+    return id;
+  }
+  
+  public int getMessageId() {
+    return messageId;
+  }
+  
+  public int getScore() {
+    return score;
+  }
+  
+  public int getNextNode() {
+    return nextNode;
+  }
+  
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("id", id)
+        .add("messageId", messageId)
+        .add("score", score)
+        .add("nextNode", nextNode)
+        .toString();
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Action action = (Action) o;
+    return id == action.id &&
+        messageId == action.messageId &&
+        score == action.score &&
+        nextNode == action.nextNode;
+  }
+  
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, messageId, score, nextNode);
+  }
+}
