4ac3330e141e759a33ee0dcd6088535f9a79014f
Rotem <rotem@faceco.de>
2020-12-30 15:57:26 +0200

    Commit more files

--- app/src/main/java/me/rootatkali/escapewp/MainActivity.java
+++ app/src/main/java/me/rootatkali/escapewp/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import me.rootatkali.escapewp.mechanics.Action;
 import me.rootatkali.escapewp.mechanics.Node;
@@ -31,21 +33,51 @@ public class MainActivity extends AppCompatActivity {
   }
   
   private void initializeNodes() {
-    Node head = new Node(0, "You started Escape. Your eyes are closed.");
+    nodes = new HashMap<>();
+    
+    Node n0 = new Node(0, "You started Escape. Your eyes are closed.")
+        .addOptions(new Action(0, R.string.n0a0, 0, "You open your eyes.", 1));
     Node n1 = new Node(1, "You are in a dark room.\n" +
-        "There is some light coming from the window. You see a light switch.");
+        "There is some light coming from the window. You see a light switch.")
+        .addOptions(new Action(0, R.string.n1a0, 5, "You flick the switch.", 2));
+    Node n2 = new Node(2, "There is a bed in the room. How didn't you see it?")
+        .addOptions(new Action(0, R.string.ellipsis, -1, null, 3));
+    Node n3 = new Node(3, "You look around the room." +
+        " There is a small desk with yellowing paperwork on it." +
+        " On the walls of the room, a horrible yellow wallpaper is plastered." +
+        " It seems to want to fall off.")
+        .addOptions(new Action(0, R.string.ellipsis, 0, null, 4));
+    Node n4 = new Node(4, "You see an old, seemingly flimsy closed wooden door.")
+        .addOptions(
+            new Action(0, R.string.n4a0, 1, "You try to open the door.", 5),
+            new Action(1, R.string.n4a1, 6, "You go to the desk.", 6)
+        );
+    Node n5 = new Node(5, "The door is locked.")
+        .addOptions(new Action(0, R.string.n4a1, 5, "You go to the desk.", 6));
+    Node n6 = new Node(6, "You see the old papers.\n" +
+        "There is a drawer. It appears to not be locked.")
+        .addOptions(
+            new Action(0, R.string.n6a0, 0, "You open the drawer.", 8),
+            new Action(0, R.string.n6a1, 0, "You look at the papers.", 7)
+        );
     
-    head.addOptions(new Action(0, R.string.n0a0, 0, 1));
-    
-    nodes.put(0, head);
-    nodes.put(1, n1);
+    nodes.put(n0.getId(), n0);
+    nodes.put(n1.getId(), n1);
+    nodes.put(n2.getId(), n2);
+    nodes.put(n3.getId(), n3);
+    nodes.put(n4.getId(), n4);
+    nodes.put(n5.getId(), n5);
+    nodes.put(n6.getId(), n6);
   }
   
   private Node next(int optionId) {
-    Action action = Arrays.stream(current.getOptions()).filter(a -> a.getId() == optionId)
-        .findFirst().orElseThrow(RuntimeException::new);
+    Action action = Arrays.stream(current.getOptions())
+        .filter(a -> a.getId() == optionId)
+        .findFirst()
+        .orElseThrow(RuntimeException::new);
     score += action.getScore();
-    Node next = nodes.get(action.getNextNode());
+    Node next = Optional.ofNullable(nodes.get(action.getNextNode()))
+        .orElseThrow(RuntimeException::new);
     
     prefs.edit()
         .putInt(getString(R.string.score_key), score)
--- app/src/main/java/me/rootatkali/escapewp/mechanics/Action.java
+++ app/src/main/java/me/rootatkali/escapewp/mechanics/Action.java
@@ -8,12 +8,14 @@ public class Action {
   private final int id;
   private final int messageId;
   private final int score;
+  private final String result;
   private final int nextNode;
   
-  public Action(int id, int messageId, int score, int nextNode) {
+  public Action(int id, int messageId, int score, String result, int nextNode) {
     this.id = id;
     this.messageId = messageId;
     this.score = score;
+    this.result = result;
     this.nextNode = nextNode;
   }
   
@@ -29,6 +31,10 @@ public class Action {
     return score;
   }
   
+  public String getResult() {
+    return result;
+  }
+  
   public int getNextNode() {
     return nextNode;
   }
@@ -39,6 +45,7 @@ public class Action {
         .add("id", id)
         .add("messageId", messageId)
         .add("score", score)
+        .add("result", result)
         .add("nextNode", nextNode)
         .toString();
   }
@@ -51,11 +58,12 @@ public class Action {
     return id == action.id &&
         messageId == action.messageId &&
         score == action.score &&
+        Objects.equals(result, action.result) &&
         nextNode == action.nextNode;
   }
   
   @Override
   public int hashCode() {
-    return Objects.hash(id, messageId, score, nextNode);
+    return Objects.hash(id, messageId, score, result, nextNode);
   }
 }
